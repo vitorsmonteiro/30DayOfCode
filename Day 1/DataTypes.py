32d26eece07cd9a2d9a58399c28be22b39322248
vitorsmonteiro <48587796+vitorsmonteiro@users.noreply.github.com>
2020-06-12 21:10:49 +0100

    Create DataTypes.py

---
+++ Day 1/DataTypes.py
@@ -0,0 +1,18 @@
+i = 4
+d = 4.0
+s = 'HackerRank '
+# Declare second integer, double, and String variables.
+
+# Read and save an integer, double, and String to your variables.
+num1 = int(input())
+num2 = float(input())
+text = input()
+
+# Print the sum of both integer variables on a new line.
+print(i + num1)
+
+# Print the sum of the double variables on a new line.
+print(d + num2)
+# Concatenate and print the String variables on a new line
+# The 's' variable above should be printed first.
+print(s + text)
